e8e2946ea0a06a91e2936b29510ba18b0fc0ec6a
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-09 04:12:14 +0300

    add main and test directories

---
+++ src/test/java/DecodeTest.java
@@ -0,0 +1,6 @@
+import org.junit.*;
+
+public class DecodeTest {
+  @Test
+  public void runCaesarCipher_alphabets_
+}
---
+++ src/test/java/EncodeTest.java
@@ -0,0 +1,7 @@
+import java.org.junit.*;
+import static org.junit.Assert.*;
+
+public class EncodeTest {
+  @Test
+  public void
+}
