ff03dba2e9ff10f76a8b41df1088c549d2508cab
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-20 09:27:54 +0300

    create tests

---
+++ src/main/java/modules/Encrypt.java
@@ -0,0 +1,79 @@
+package modules;
+
+public class EncryptDecrypt {
+  private int key;
+  private String text;
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public int getKey() {
+    return key;
+  }
+
+  public void setKey(int key) {
+    this.key = key;
+  }
+
+  public String Encrypt(int key, String text) {
+    this.key = key;
+    this.text = text;
+
+    String encryptedText = "";
+
+
+    if (key == 0 && !text.isEmpty()) {
+      encryptedText = this.text;
+    } else if (text.isEmpty() && key >= 0 && key <= 25) {
+      encryptedText = "No text to encrypt,Enter text";
+    } else if ((text.isEmpty() || !text.isEmpty()) && (key < 0 || key > 25)) {
+      encryptedText = "Key must be a number between 0 and 25";
+    }
+
+    char[] aCharacters = this.text.toCharArray();
+
+    for (char character : aCharacters) {
+      if (!text.isEmpty() && key >= 1 && key <= 25) {
+        int valueOfChar = Character.valueOf(character) + key;
+        char shifted = (char) valueOfChar;
+
+        encryptedText += Character.toString(shifted);
+      }
+    }
+    return encryptedText;
+  }
+  public String Decrypt(int key, String text) {
+    this.key = key;
+    this.text = text;
+
+    String decryptedText = "";
+
+
+    if (key == 0 && !text.isEmpty()) {
+      decryptedText = this.text;
+    } else if (text.isEmpty() && key >= 0 && key <= 25) {
+      decryptedText = "No text to encrypt,Enter text";
+    } else if ((text.isEmpty() || !text.isEmpty()) && (key < 0 || key > 25)) {
+      decryptedText = "Key must be a number between 0 and 25";
+    }
+
+    char[] aCharacters = this.text.toCharArray();
+
+    for (char character : aCharacters) {
+      if (!text.isEmpty() && key >= 1 && key <= 25) {
+        int valueOfChar = Character.valueOf(character) - key;
+        char shifted = (char) valueOfChar;
+
+        decryptedText += Character.toString(shifted);
+      }
+    }
+    return decryptedText;
+  }
+}
+
+
--- src/test/java/EncodeTest.java
+++
@@ -1,7 +0,0 @@
-import java.org.junit.*;
-import static org.junit.Assert.*;
-
-public class EncodeTest {
-  @Test
-  public void
-}
