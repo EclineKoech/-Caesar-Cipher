6a06a094deee6c3ea94005ef0113d1e6b34a833f
Ecline Koech <eclinejerutokoech@gmail.com>
2021-10-25 10:48:20 +0300

    add test class

--- src/test/java/modules/DecryptTest.java
+++ src/test/java/modules/DecryptTest.java
@@ -1,25 +1,4 @@
 package modules;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class DecryptTest {
-  @Test
-  public void encryptDecryptClassExists() {
-    Encrypt encryptDecrypt = new Encrypt();
-    assertEquals(true,encryptDecrypt instanceof Encrypt);
-  }
-
-  @Test
-  public void encryptsCharacter(){
-    Encrypt encryptDecrypt = new Encrypt();
-    assertEquals("a",encryptDecrypt.Encrypt(1,"b"));
-  }
-
-  @Test
-  public void encryptsSentence() {
-    Encrypt encryptDecrypt = new Encrypt();
-    assertEquals("dog", encryptDecrypt.Encrypt(2, "fqi"));
-  }
 }
